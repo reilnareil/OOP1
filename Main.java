9ae996506d44482b416d44f3d6e9af611a86b308
Reilan Cad <reilancadubla@gmail.com>
2023-09-05 11:40:03 +0800

    init

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person p = new Person("Franco", "Masbate", 150, 20);
+        System.out.print(p.IQ);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,13 @@
+
+public class Person {
+
+	String name, address;
+	int IQ, age;
+	Person(String name, String address, int IQ, int age){
+		this.name = name;
+		this.address = address;
+		this.IQ = IQ;
+		this.age = age;
+		
+	}
+}
